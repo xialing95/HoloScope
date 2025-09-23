88d9af997c84f86b769648c95d93e9fa5cc7d4e8
xialing95 <xialing95@users.noreply.github.com>
2025-09-22 23:46:52 -0400

    test

--- app/camera/routes.py
+++ app/camera/routes.py
@@ -141,7 +141,7 @@ def index():
     return render_template('camera.html')
 
 # This tells Flask to serve files from this directory under the /holoscope_images/ URL
-@camera_bp.route('/camera/<path:filename>')
+@camera_bp.route('/<path:filename>')
 def serve_holoscope_images(filename):
     # Make sure 'capture_image_dir' is defined in your app's configuration
     return send_from_directory(current_app.config['CAPTURE_IMAGE_DIR'], filename)   
