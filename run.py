8a2fc184e9a3ebe853f8ea9184fcc5cb192010c2
xialing95 <xialing95@users.noreply.github.com>
2025-09-24 12:58:18 -0400

    test

--- run.py
+++ run.py
@@ -7,4 +7,4 @@ if __name__ == '__main__':
     # making it accessible from other computers on the same network.
     # debug=True allows for automatic reloading and provides a debugger.
     app = create_app()
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True, threaded=False, processes=1)
