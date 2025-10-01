66e14365fbbcdbd06a1fa4b17048077396ccdfb8
xialing95 <xialing95@users.noreply.github.com>
2025-10-01 15:47:27 -0400

    test

--- run.py
+++ run.py
@@ -101,7 +101,7 @@ if __name__ == '__main__':
             app.run(
                 host='0.0.0.0', 
                 port=PORT, 
-                debug=True, 
+                debug=False,
                 threaded=False, 
                 processes=1
             )
