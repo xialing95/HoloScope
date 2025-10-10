b0b075d7fcd43cd8c98522cc4d4b8d1e9f22012a
xialing95 <xialing95@users.noreply.github.com>
2025-10-09 23:29:21 -0400

    test

--- run.py
+++ run.py
@@ -7,8 +7,6 @@ from flask import Flask
 # Assuming 'app' is a folder and 'create_app' is a function inside 'app/__init__.py'
 from app import create_app 
 import app.epaper_display.epaper_service as epd_service
-import app.epaper_display.epd2in13b_V4
-import app.epaper_display.epdconfig
 
 PORT = 8080
 
